6e077a85ee23447ba60dd9483428587ac006d58e
Karol Vargas <vargask@allegheny.edu>
2018-09-20 14:14:53 -0400

    header photo

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
     title: 'Karol Vargas', // Required
-    author: 'Allegheny College 2019', // Required
-    tagline: 'Full-stack Web Developer',
+    author: 'Karol Alberto Vargas', // Required
+    tagline: 'Miami, FL Full-Stack Developer',
     primaryColor: '#3498db', // Required
     showHeaderImage: true,
     showShareButtons: true,
     postsPerPage: 5, // Required
     social: {
-        website: 'https://ryanfitzgerald.ca',
+        website: 'https://alleghenycollege.edu',
         github: 'https://github.com/karolvargas',
         twitter: 'https://twitter.com/karolavargas',
         linkedin: 'https://www.linkedin.com/in/karolalbertovargas/'
